0aaccd0322b7bc808003307af6aece0d896e19cd
jrios <jrios@ejemplo.com>
2026-09-23 19:30:04 -0500

    chore: keep only README on main

--- customer-service/src/main/java/com/bank/customerservice/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bank.customerservice.repository;
-
-import com.bank.customerservice.model.Customer;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends ReactiveMongoRepository<Customer, String> {
-}
