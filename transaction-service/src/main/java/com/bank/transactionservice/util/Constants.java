cc34edfc1679f160d442b6d9fbb24d5aacd4a736
jrios <jrios@ejemplo.com>
2026-09-23 20:58:00 -0500

    fix: correctly rewrite Constants in transaction-service

--- transaction-service/src/main/java/com/bank/transactionservice/util/Constants.java
+++ transaction-service/src/main/java/com/bank/transactionservice/util/Constants.java
@@ -1,14 +1,10 @@
 package com.bank.transactionservice.util;
 
 public final class Constants {
-    private Constants() {
-        // Restringe la instanciaciÃ³n
-        public static final int LATEST_TRANSACTIONS_LIMIT = 10;
-}
+    private Constants() {}
 
     public static final String NOT_FOUND_MSG = "Registro no encontrado";
-    public static final String SUCCESS_MSG = "OperaciÃ³n realizada con Ã©xito";
-    public static final String ERROR_MSG = "OcurriÃ³ un error inesperado";
+    public static final String SUCCESS_MSG = "Operacion realizada con exito";
+    public static final String ERROR_MSG = "Ocurrio un error inesperado";
     public static final int LATEST_TRANSACTIONS_LIMIT = 10;
 }
-
